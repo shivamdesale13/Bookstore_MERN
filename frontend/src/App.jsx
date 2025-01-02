36436e53c56e4ed6b6269c16f4f0d8968374056d
Shivam Desale <shivamdesale@Mac.lan>
2025-01-01 20:01:56 -0800

    New

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
+
+function App(){
+  return(<>
+    <Navbar></Navbar>
+    <Banner></Banner>
+    <Footer></Footer>
+  </>)
+}
+
+export default App
